c8bece135e144b4ff4db8acfe9e9ba61a7d74ee5
pompom poto <48266828+jjoohee@users.noreply.github.com>
2020-09-08 05:33:04 +0900

    Add files via upload

---
+++ doong-js.js
@@ -0,0 +1,32 @@
+//흉둥이
+
+human.onclick = function(){
+	human.style.opacity = "0%";
+	human_1.style.opacity = "100%";
+}
+
+human1_1.onclick = function(){
+	human1_1.style.opacity = "100%";
+}
+
+
+//human1-.onclick = function(){	bodyy.style.position = "relative";}
+
+//스크롤시 다른 링크로 이동
+
+   $(window).scroll(function() {
+        var scrolltop = $(document).scrollTop();
+        console.log(scrolltop);
+        var height = $(document).height();
+        console.log(height);
+        var height_win = $(window).height();
+        console.log(height_win);
+        
+        
+     if (Math.round( $(window).scrollTop()) == $(document).height() - $(window).height()) {
+        
+       // var url = "https://jjoohee.github.io/doong0826/";
+		$(location).attr('href',url);
+        
+    }
+});
