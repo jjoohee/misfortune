37b6f08704e2a1aa309fbc7b429a181b198a33b0
pompom poto <48266828+jjoohee@users.noreply.github.com>
2020-09-22 14:55:22 +0900

    Update doong-js.js

--- doong-js.js
+++ doong-js.js
@@ -1,16 +1,60 @@
-//흉둥이
 
-human.onclick = function(){
-	human.style.opacity = "0%";
-	human_1.style.opacity = "100%";
-}
+window.onscroll = function(){
+        var t =document.documentElement.scrollTop + document.body.scrollTop;
+        moniter.innerHTML = t
 
-human1_1.onclick = function(){
-	human1_1.style.opacity = "100%";
-}
+            if (t > 2800) {
+            human1_1.style.opacity = "100%";
 
+            }
+            else{
+                 human1_1.style.opacity = "";
+            }
 
-//human1-.onclick = function(){	bodyy.style.position = "relative";}
+            if (t > 3400) {
+            hand1.style.opacity = "0%";
+            hand1.style.right = "300px";
+            hand1.style.top = "3150px";
+            hand1.style.transform = "rotate(-10deg)";
+
+            hand2.style.opacity = "0%";
+            hand2.style.right = "100px";
+            hand2.style.top = "3000px";
+            hand2.style.transform = "rotate(-30deg)";
+
+            hand1_1.style.opacity = "0%";
+            hand1_1.style.left = "250px";
+            hand1_1.style.top = "3100px";
+            hand1_1.style.transform = "rotate(20deg)";
+
+            hand3.style.opacity = "0%";
+            hand3.style.left = "100px";
+            hand3.style.top = "3000px";
+            hand3.style.transform = "rotate(-30deg)";
+
+            }
+            else{
+            hand1.style.opacity = "";
+            hand1.style.right = "";
+            hand1.style.top = "";
+            hand1.style.transform = "";
+
+            hand2.style.opacity = "";
+            hand2.style.right = "";
+            hand2.style.top = "";
+            hand2.style.transform = "";
+
+            hand1_1.style.opacity = "";
+            hand1_1.style.left = "";
+            hand1_1.style.top = "";
+            hand1_1.style.transform = "";
+
+            hand3.style.opacity = "";
+            hand3.style.left = "";
+            hand3.style.top = "";
+            hand3.style.transform = "";
+            }
+    }    
 
 //스크롤시 다른 링크로 이동
 
